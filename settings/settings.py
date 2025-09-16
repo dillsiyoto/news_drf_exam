efbe4883d82780e98d65739eddf908b5afc199ec
Adilya Amanbay <adilya.amanbay@gmail.com>
2025-09-16 22:54:01 +0500

    users

--- settings/settings.py
+++ settings/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Almaty"
 
 USE_I18N = True
 
@@ -120,3 +120,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.Client"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from dirtyfields import DirtyFieldsMixin
@@ -9,3 +11,23 @@ class Client(DirtyFieldsMixin, AbstractUser):
         max_length=30,
         unique=True
     )
+    is_active = models.BooleanField(
+        verbose_name="активированный аккаунт", 
+        default=False
+    )
+    activation_code = models.UUIDField(
+        verbose_name="код активации", 
+        unique=True, 
+        default=uuid.uuid4
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        ordering = ("id",)
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
+
+    def __str__(self):
+        return f"{self.pk} -> {self.email}"
